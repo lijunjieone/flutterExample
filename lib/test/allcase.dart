ef2b15f85fa040688ddfbf5ab2e3c8112ebf9e19
lijunjie <lijunjieone@gmail.com>
2018-10-09 10:56:01 +0800

    增加测试方案

--- lib/test/allcase.dart
+++ lib/test/allcase.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
-
 class CaseList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -10,14 +7,35 @@ class CaseList extends StatefulWidget {
   }
 }
 
-
 class _CaseListState extends State<CaseList> {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      child: new Text(
-        "text",
-
+      child: new Center(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Card(
+              child: new ListTile(
+                title: new Text("test1"),
+                onTap: () {
+                  print("test1");
+                },
+              ),
+            ),
+            new Card(
+              child: new ListTile(
+                title: new Text(
+                  "test2",
+                  style: new TextStyle(fontSize: 20.0, color: Colors.blue),
+                ),
+                onTap: () {
+                  print("test2");
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
